457f619db6e33dc2632b227694b8bf7b574566d2
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2018-10-05 13:22:13 -0700

    Updated Unity Project

---
+++ Lit/Assets/_Scripts/Britney_time_test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Britney_time_test : MonoBehaviour {
+   
+    [SerializeField] 
+    private float time;
+    [SerializeField] 
+    private int healPoints;
+	// Use this for initialization
+	 void Start () 
+    {
+        Debug.Log("Begin");
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+        if(time>= 0.0f)
+        {
+            time -= Time.deltaTime;
+        }
+        else if (time <= 0.0f)
+        {
+            GameOver();
+        }
+		
+	}
+    void GameOver()
+    {
+        Debug.Log("Dead :P");
+
+        // new scene implemented
+    }
+}
