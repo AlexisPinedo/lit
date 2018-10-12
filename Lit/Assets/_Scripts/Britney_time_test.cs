b057efca4a66189a72b75a784378792aaeff9223
Britney Khuu <britneykhuu@gmail.com>
2018-10-12 14:01:15 -0700

    made some scenes; mainMenu can be altered, just place holders currently loadscene code is self explanatory the folder of britney test objects is there for testing my code

--- Lit/Assets/_Scripts/Britney_time_test.cs
+++ Lit/Assets/_Scripts/Britney_time_test.cs
@@ -1,37 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-
-public class Britney_time_test : MonoBehaviour {
+public class Britney_time_test : MonoBehaviour
+{
    
-    [SerializeField] 
+
+    [SerializeField]
     private float time;
-    [SerializeField] 
+    [SerializeField]
     private int healPoints;
-	// Use this for initialization
-	 void Start () 
+    // Use this for initialization
+   
+    void Start()
     {
         Debug.Log("Begin");
-	}
-	
-	// Update is called once per frame
-	void Update() 
+    }
+
+    // Update is called once per frame 
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if(time>= 0.0f)
+       if (collision.collider.tag == "Danger" )
+       {
+            Debug.Log("Hits danger.");
+            healPoints--;
+       }
+    }
+    void Update()
+    {
+
+        if (time >= 0.0f)
         {
             time -= Time.deltaTime;
         }
-        else if (time <= 0.0f)
+        else if (healPoints <= 0.0f || time <= 0.0f)
         {
+            Debug.Log("Made it into the else if");
+            time = 0;
+            healPoints = 0;
             GameOver();
         }
-		
-	}
+    }
     void GameOver()
     {
         Debug.Log("Dead :P");
+        SceneManager.LoadScene("Khuu_Britney_Test");
 
         // new scene implemented
     }
+   
 }
---
+++ Lit/Assets/_Scripts/Loadscene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadscene : MonoBehaviour
+{
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
