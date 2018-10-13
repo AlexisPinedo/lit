efa29ea7f1d1d7c7d8de2bff7f1a0f5ac70abcd6
DESKTOP-BHVRHRV\Steven <todasteven1@gmail.com>
2018-10-12 20:08:09 -0700

    added portal and moving platform mechanics

---
+++ Lit/Assets/_Scripts/SimpleAnime.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnime : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField] Vector3 pos;
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveToTest();
+    }
+
+    void MoveToTest()
+    {
+        iTween.MoveTo(this.gameObject, iTween.Hash("position", pos, "time", 5f, "easetype", iTween.EaseType.easeInOutSine, "looptype", iTween.LoopType.pingPong) );
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+        }
+    }
+
+
+    
+}
