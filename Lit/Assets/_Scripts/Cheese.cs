68058a46ac6339605a5113ea5ca3ca7d4fde43e4
DESKTOP-BHVRHRV\Steven <todasteven1@gmail.com>
2018-12-01 12:56:28 -0800

    Added Cheese collectible and Mouse Obstacle

---
+++ Lit/Assets/_Scripts/Cheese.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheese : MonoBehaviour {
+    public void OnTriggerEnter2D(Collider2D vari)
+    {
+        if (vari.gameObject.tag == "Player")
+        {
+            Debug.Log("Cheese has been found.");
+            Mouse.incrementCheese();
+            Destroy( gameObject );
+        }
+    }
+}
