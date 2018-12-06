593056b59c9a7cf08c6dd986c480161909f2a73e
Britney Khuu <britneykhuu@gmail.com>
2018-12-06 14:34:18 -0800

    added fading animation and animator it has problems transitioning in main menu made fade prefab

---
+++ Lit/Assets/_Scripts/SceneChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+    // Use this for initialization
+    public Animator animator;
+    private int levelLoad;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+    public void FadetoLevel(int LevelIntex)
+    {
+       levelLoad = LevelIntex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelLoad);
+    }
+}
